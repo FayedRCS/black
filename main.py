30b930e7a45c9cf7b2cf5d7d4372b0e7a252781b
fayed <fayed_rana@outlook.com>
2024-09-03 20:26:13 +0200

    display and framerate

--- main.py
+++ main.py
@@ -5,28 +5,28 @@ import pygame # type: ignore
 from constants import *
 
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
 
     pygame.init
-
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    i = True
+    clocka = pygame.time.Clock()
+    dt = 0
 
-    while i == True:
-        screen.fill(000000)
+
+    while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        screen.fill(000000)
+        pygame.display.flip()
+        
+        dt = clocka.tick(60) / 1000
 
 
-    pygame.display.flip()     
 
 if __name__ == "__main__":
     main()
