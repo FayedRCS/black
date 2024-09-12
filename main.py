61dc6c60f01b169957ae4c0ccaf3ff77fb3c5699
fayed <fayed_rana@outlook.com>
2024-09-12 02:55:59 +0200

    functioning player movements

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-import pygame # type: ignore
+import pygame
 from constants import *
-
+from player import Player
 
 
 def main():
-
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-    clocka = pygame.time.Clock()
+    clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable.add(player)
+    drawable.add(player)
+    
 
     while True:
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill(000000)
-        pygame.display.flip()
-        
-        dt = clocka.tick(60) / 1000
+        #updates
+        for i in updatable:
+            i.update(dt)
 
+        #rendering
+
+        screen.fill("black")
+        for i in drawable:
+            i.draw(screen)
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS / controling framerate
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
     main()
-
